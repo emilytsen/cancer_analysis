28f1fd997edd0f568fa039ddda7eec6bb3ca7353
emilytsen <emilygabrielat@gmail.com>
2023-10-23 14:31:52 -0300

    add streamlit

---
+++ browser/app.py
@@ -0,0 +1,43 @@
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st 
+
+# app config 
+# emojis linmk: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Cancer Analisys",
+                   page_icon=":bar_chart:",
+                   layout="wide"
+                   )
+
+
+df = pd.read_csv('/Users/emilytsen/Documents/courses/ds_cancer/df/df_countrys.csv')
+
+#st.dataframe(df)
+
+#sidebar config
+
+st.sidebar.header("Filter here:")
+
+country = st.sidebar.multiselect(
+    "Select The Country:",
+    options=df["COUNTRY"].unique()
+)
+
+year = st.sidebar.multiselect(
+    "Select The Year:",
+    options=df["YEAR"].unique(),
+    default=df["YEAR"].unique()
+)
+
+df_selection = df.query(
+    "COUNTRY == @country & YEAR == @year"
+)
+
+st.dataframe(df_selection)
+
+# main page:
+
+st.title(":bar_chart: Cancer Analisys")
+st.markdown("##")
+
